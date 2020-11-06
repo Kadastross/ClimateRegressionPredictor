b84fd2cbd5c0cfc3947fbaa25f2de5e8e125bc4b
isadash <anchald2@illinois.edu>
2020-11-05 18:52:50 -0800

    initial react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          CS 411 Climate Change Project
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
