d375fa1c8ca69bcd2de3acac112d7d5a005d3e40
Mohamed Amn <mamn2@illinois.edu>
2020-11-29 12:47:49 -0600

    mongo and SQL databases online

--- Database/importCO2Data.py
+++ Database/importCO2Data.py
@@ -1,11 +1,38 @@
+'''
+
+!!!!!!! PLEASE READ !!!!!!!
+
+This file is only run one time in order to initialize the MongoDB database with the CO2 Emissions Data
+
+'''
+
 import csv
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb+srv://cs411group40:cs411group40@cluster0.gydem.mongodb.net/<dbname>?retryWrites=true&w=majority")
+EmissionsData = myclient['EmissionsData']
+CO2Emissions = EmissionsData['CO2Emissions']
+
+with open('co2_emissions.csv') as co2_data:
+    csv_reader = csv.reader(co2_data, delimiter=',')
+    next(csv_reader) # skip first line
+    for row in csv_reader:
+        if len(row[0].strip()) is 0 or len(row[1].strip()) is 0 or len(row[2].strip()) is 0 or len(row[8].strip()) is 0:
+            continue
+        print("Country:", row[0])
+        print("Year", int(row[1]))
+        print("AnnualCO2Emissions", float(row[2]) * 1000000) # million tons
+        print("AnnualCO2EmissionsPerCapita:", float(row[8]) * 1000000000000) # tons
+        toInsert = {"Country": row[0], "Year": int(row[1]), "AnnualCO2Emissions": float(row[2]), "AnnualCO2EmissionsPerCapita": float(row[8])}
+        x = CO2Emissions.insert_one(toInsert)
+
+'''
 import mysql.connector
 
-dbIP = "localhost"
-dbUser = "root"
-dbPassword = "password"
-dbName = 'ClimatePredictorDatabase'
-charSet = "utf8mb4"
+dbIP = "sql9.freemysqlhosting.net"
+dbUser = "sql9379139"
+dbPassword = "RKBNi5PlkS"
+dbName = 'sql9379139'
 
 db = mysql.connector.connect(host = dbIP,
                                      user = dbUser,
@@ -27,3 +54,4 @@ with open('co2_emissions.csv') as co2_data:
         cursor.execute('INSERT INTO CO2_Emissions(Country, Year, AnnualCO2Emissions, AnnualCO2EmissionsPerCapita)' \
                        'VALUES("%s", "%s", "%s", "%s")', (row[0], int(row[1]), float(row[2]), float(row[8])))
         db.commit()
+'''
