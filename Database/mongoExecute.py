e0046b8f3b358b3f83bff09de0d74f82bf509a0a
Mohamed Amn <mamn2@illinois.edu>
2020-11-29 19:23:59 -0600

    added flask for mongo data for heatmap'

--- Database/mongoExecute.py
+++ Database/mongoExecute.py
@@ -1,4 +1,6 @@
-import pymongo, flask, atexit
+import pymongo, atexit
+from flask import Flask
+from flask_cors import CORS
 
 myclient = pymongo.MongoClient("mongodb+srv://cs411group40:cs411group40@cluster0.gydem.mongodb.net/<dbname>?retryWrites=true&w=majority")
 EmissionsData = myclient['EmissionsData']
@@ -10,6 +12,17 @@ def exit_handler():
 atexit.register(exit_handler)
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+@app.route('/retrieveData', methods=['GET', 'POST'])
+def retrieveData():
+    CO2Emissions.find()
+    cursor = collection.find({})
+    toReturn = []
+    for document in cursor:
+        toReturn.append(document)
+    return toReturn
+
 
 if __name__ == "__main__":
     app.run()
