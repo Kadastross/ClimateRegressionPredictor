1ed5e020cc66d67d7940c9dff1ba5424f1529fe7
Mohamed Amn <mamn2@illinois.edu>
2020-12-02 00:42:39 -0600

    cleanup database files

--- Database/updateCO2Data.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb+srv://cs411group40:cs411group40@cluster0.gydem.mongodb.net/<dbname>?retryWrites=true&w=majority")
-EmissionsData = myclient['EmissionsData']
-CO2Emissions = EmissionsData['CO2Emissions']
-
-
-print(countryCodeMap)
